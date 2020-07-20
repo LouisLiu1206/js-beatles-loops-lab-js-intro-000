9cebc9c4cd32b8251a07f5678b5a5b2589875153
Qize <louisliu1206@yahoo.com>
2020-07-20 02:32:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,20 +6,12 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts() {
-  var facts = [
- "He was the last Beatle to learn to drive",
- "He was never a vegetarian",
- "He was a choir boy and boy scout",
- "He hated the sound of his own voice",
- "While on tour, he enjoyed playing Monopoly",
- "Harmonica was the first instrument Lennon learned to play",
-];
-  var array = [];
-  var i = 0;
-  while (array.length < facts.length) {
-    array.push(facts[i] + "!!!");
-    i++;
+ffunction johnLennonFacts(facts){
+  let i = 0;
+  while(i < facts.length){
+    facts[i] = facts[i] + "!!!"
+    i++
   }
-  return array;
+
+  return facts
 }
