f1a0b5aca3afbe9e2c361dfbc1141d6c62e57dbb
Qize <louisliu1206@yahoo.com>
2020-07-20 02:44:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function johnLennonFacts(facts){
   var array = [];
   let i = 0;
   while(i < facts.length){
-    array.push(${facts[1]} + "!!!")
+    array.push(${facts[i]} + "!!!")
     i++;
   }
   return array;
