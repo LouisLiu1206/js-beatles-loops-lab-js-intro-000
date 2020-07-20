65bcbc86f7da6a953637d5ba6e4de7ec293282c7
Qize <louisliu1206@yahoo.com>
2020-07-20 02:43:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 
-const facts = [
+let facts = [
 "He was the last Beatle to learn to drive",
 "He was never a vegetarian",
 "He was a choir boy and boy scout",
