70be41b2c786ae87f12b8f79556e779409661f65
Qize <louisliu1206@yahoo.com>
2020-07-20 02:27:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,16 +6,13 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-function johnLennonFacts(facts) {
-  var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-let i = 0;
-while (i < facts.length) {
-  i++;
-  return facts[i] + "!!!"
-}
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var array = [];
+  var i = 0;
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++;
+  }
+  return array;
 }
