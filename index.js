3106b9096395db80512b8d5cee010105daff5b92
Qize <louisliu1206@yahoo.com>
2020-07-20 02:24:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
+
+function johnLennonFacts(facts) {
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+let i = 0;
+while (i < facts.length) {
+  i++;
+  return fact[i] + "!!!"
+}
+}
